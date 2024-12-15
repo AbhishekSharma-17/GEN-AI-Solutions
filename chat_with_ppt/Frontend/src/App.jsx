dcfd1679808bc379532943264ce61909e5043c2d
ONLY1gb <gautam.bhawsar8269@gmail.com>
2024-12-15 22:36:36 +0530

    Gautam commit

--- chat_with_ppt/Frontend/src/App.jsx
+++ chat_with_ppt/Frontend/src/App.jsx
@@ -4,17 +4,17 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Main from "./Components/Main/Main";
 import Sidebar from "./Components/SideBar/Sidebar";
-import HomePage from "./Components/HomePage/HomePage";
 import { Context } from "./context/Context";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import HomePageContainer from "./Components/HomePage Container/HomePageContainer";
 
 const App = () => {
   const { initialisationStatus } = useContext(Context);
 
   return (
     <>
-      {!initialisationStatus ? <HomePage /> : (
+      {!initialisationStatus ? <HomePageContainer /> : (
         <>
           <Sidebar />
           <Main />
