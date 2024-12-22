98ba1666d01160af1fd7eb900a616b41cad01a16
Viplove Parsai <viploveparsai1@gmail.com>
2024-12-22 17:37:40 +0530

    Tokens display functionality added.

--- chat_with_sql/Frontend/src/Components/Sidebar/Sidebar.jsx
+++ chat_with_sql/Frontend/src/Components/Sidebar/Sidebar.jsx
@@ -56,8 +56,7 @@ const Sidebar = () => {
       if (data.answer) {
         setAnswer(data.answer);
       }
-      setUserQuestion('');
-
+      setUserQuestion("");
     } catch (error) {
       console.error("Error:", error);
       setError(
@@ -69,6 +68,83 @@ const Sidebar = () => {
   };
   return (
     <div className="main-sidebar">
+      {/* token display starts here */}
+      <div className="token-display">
+        <div className="latency">
+          <p>Latency :</p>
+        </div>
+        <div className="tokens">
+          <p>Tokens :</p>
+        </div>
+
+        {/* hover content starts here */}
+        <div class="hover-content">
+          <div className="speed-insights">
+            <p>Speed Insights</p>
+          </div>
+
+          {/*input output token starts   */}
+          <div style={{ padding: "10px" }}>
+            <p className="token-details-title">Tokens </p>
+            <div className="input-output-token">
+              <div className="input-token">
+                <span className="token-value">51</span>
+                <span className="token-title">Input token</span>
+              </div>
+              <div className="output-token">
+                <span className="token-value">49</span>
+                <span className="token-title">Output token</span>
+              </div>
+              <div className="total-token">
+                <span className="token-value">100</span>
+                <span className="token-title">Total token</span>
+              </div>
+            </div>
+          </div>
+          {/*  input out token end*/}
+          {/* inference starts  */}
+          <div style={{ padding: "10px" }}>
+            <p className="token-details-title">Inference Time </p>
+            <div className="inference-time">
+              <div className="input-inference">
+                <span className="token-value">51</span>
+                <span className="token-title">Input seconds</span>
+              </div>
+              <div className="output-inference">
+                <span className="token-value">49</span>
+                <span className="token-title">Output seconds</span>
+              </div>
+              <div className="total-inference">
+                <span className="token-value">100</span>
+                <span className="token-title">Total seconds</span>
+              </div>
+            </div>
+          </div>
+          {/*  inference ends*/}
+          {/* token per second starts */}
+          <div
+            style={{ padding: "10px", backgroundColor: "rgb(245, 245, 245)" }}
+          >
+            <p className="token-details-title">Token / Seconds </p>
+            <div className="token-per-second">
+              <div className="input-token-per-second">
+                <span className="token-value">51</span>
+                <span className="token-title">Input seconds</span>
+              </div>
+              <div className="output-token-per-second">
+                <span className="token-value">49</span>
+                <span className="token-title">Output seconds</span>
+              </div>
+              <div className="total-token-per-second">
+                <span className="token-value">100</span>
+                <span className="token-title">Total seconds</span>
+              </div>
+            </div>
+          </div>
+          {/* token per second ends */}
+        </div>
+      </div>
+      {/* token display ends here */}
       <div className="main-sidebar-top">
         <p className="sidebar-top-title">Recent Queries</p>
         <div className="queries-list">
