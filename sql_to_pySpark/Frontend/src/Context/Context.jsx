decb42191beb0ed79ac0472ca5fba1bf5ed8d5fc
Viplove Parsai <viploveparsai1@gmail.com>
2025-01-02 15:00:00 +0530

    Sql to PySpark Project Frontend Setup.

---
+++ sql_to_pySpark/Frontend/src/Context/Context.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+export const Context = createContext();
+
+const ContextProvider = (props) => {
+  const [sample, setSample] = useState();
+
+  const ContextValue = {
+    sample,
+    setSample,
+  };
+
+  return (
+    <Context.Provider value={ContextValue}>{props.children}</Context.Provider>
+  )
+};
+
+export default ContextProvider;
