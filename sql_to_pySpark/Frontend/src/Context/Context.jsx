669b8f932fb74741e71808662456ed8398e8f533
Viplove Parsai <viploveparsai1@gmail.com>
2025-01-03 16:13:16 +0530

    File upload functionality Completed.

--- sql_to_pySpark/Frontend/src/Context/Context.jsx
+++ sql_to_pySpark/Frontend/src/Context/Context.jsx
@@ -6,9 +6,39 @@ const ContextProvider = (props) => {
   // provider
   const [selectedProvider, setSelectedProvider] = useState();
 
+  // uploaded file
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+
+  // model choice
+  const [providerChoice, setProviderChoice] = useState("ChatGPT");
+
+  // API Key
+  const [apiKey, setApiKey] = useState("");
+
+  // modelName
+  const [modelName, setModelName] = useState("");
+
+  // Conversion Result
+  const [conversionResults, setConversionResults] = useState([]);
+
+  // loading state
+  const [loading, setLoading] = useState(false);
+
   const ContextValue = {
     selectedProvider,
     setSelectedProvider,
+    uploadedFiles,
+    setUploadedFiles,
+    providerChoice,
+    setProviderChoice,
+    apiKey,
+    setApiKey,
+    modelName,
+    setModelName,
+    conversionResults,
+    setConversionResults,
+    loading,
+    setLoading,
   };
 
   return (
