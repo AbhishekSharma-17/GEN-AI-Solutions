d6fcba730b3a156476ff3664dc1e4081b4514351
Viplove Parsai <viploveparsai1@gmail.com>
2025-01-05 19:31:19 +0530

    Update frontend for voice chat application: change title to "Voice Chat", add Bootstrap 5.3.3 for styling, and restructure App component to include Sidebar and Content components. Refactor CSS for improved layout and responsiveness.

---
+++ voice_conversation/frontend/src/Components/Content/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Content.css'
+const Content = () => {
+  return (
+    <div className='content'>
+      <div className='chat-section container'></div>
+    </div>
+  )
+}
+
+export default Content
