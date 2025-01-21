c21ef0c039664db03f782b92b7e9bf7480f745a1
Viplove Parsai <viploveparsai1@gmail.com>
2025-01-21 23:30:28 +0530

    Done With UI Changes.

---
+++ advance_pdf/Frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./App.css";
+import './index.css';
+import Navbar from "./Components/Navbar/Navbar";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Chat from "./Components/Chat/Chat";
+import Upload from "./Components/Upload/Upload";
+const App = () => {
+  return (
+    <div className="main-app">
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <div className="main-content">
+        <div className="sidebar">
+          <Sidebar></Sidebar>
+        </div>
+        <div className="upload">
+          <Upload></Upload>
+        </div>
+        <div className="chat">
+          <Chat></Chat>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ advance_pdf/Frontend/src/Components/Upload/Upload.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Upload.css";
+
+const Upload = () => {
+  return (
+    <div className="actual-upload">
+      <div className="prototype-name">
+        <p>Chat With PDF</p>
+      </div>
+      <div className="file-upload">{/* upload all icon and text here  */}</div>
+    </div>
+  );
+};
+
+export default Upload;
