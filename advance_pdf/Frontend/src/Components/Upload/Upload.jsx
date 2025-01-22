5720e3c43235c5c204c3d93b784606cdae6ae67c
ONLY1gb <gautam.bhawsar8269@gmail.com>
2025-01-22 22:18:43 +0530

    upload file

--- advance_pdf/Frontend/src/Components/Upload/Upload.jsx
+++ advance_pdf/Frontend/src/Components/Upload/Upload.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Upload.css";
+import { FiUpload } from "react-icons/fi";
 
 const Upload = () => {
   return (
@@ -7,7 +8,21 @@ const Upload = () => {
       <div className="prototype-name">
         <p>Chat With PDF</p>
       </div>
-      <div className="file-upload">{/* upload all icon and text here  */}</div>
+      <div className="file-upload">{/* upload all icon and text here  */}
+
+          <label htmlFor="file-upload" className="select-button">
+            <FiUpload style={{ fontSize: "50px" }} />
+          </label>
+          <h3>Upload your PDF</h3>
+          <p>Drag and drop your file here, or click to select</p>
+          <input
+            type="file"
+            accept="application/pdf"
+            style={{ display: "none" }}
+            id="file-upload"
+          />
+
+      </div>
     </div>
   );
 };
