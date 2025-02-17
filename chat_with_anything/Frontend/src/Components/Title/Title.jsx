b8df377d1d725bfc3abfe62cb5f535251a985daa
ONLY1gb <gautam.bhawsar8269@gmail.com>
2025-02-18 00:26:38 +0530

    document

--- chat_with_anything/Frontend/src/Components/Title/Title.jsx
+++ chat_with_anything/Frontend/src/Components/Title/Title.jsx
@@ -4,7 +4,7 @@ import './Title.css';
 const Title = () => {
   return (
     <div className='anyDoc-title'>
-      <h1>Chat With Any Documents</h1>
+      <h1>Chat With Any Document</h1>
       <p className='anydoc-title-tagline'>
         Transform the way you work with documents. Summarize, analyze, and extract insights from various file types including PDFs, spreadsheets, images, and more. Simplify complex data and make informed decisions quickly and efficiently, all in one seamless platform designed to meet your document management needs.
       </p>
